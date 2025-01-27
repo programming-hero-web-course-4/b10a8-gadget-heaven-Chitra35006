72a5f32ed9c47115b07067c6a361ad61f4f8e05c
Chitra35006 <ch18.cse@gmail.com>
2025-01-27 23:04:55 +0600

    intializing tailwind,daisy ui, product & category json file, root route

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,17 @@
+import  { Toaster } from 'react-hot-toast';
+import NavBar from '../Components/NavBar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Footer';
+
+const Root = () => {
+    return (
+        <div>
+            <Toaster></Toaster>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
